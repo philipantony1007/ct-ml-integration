b44bbfd644d787fb52abfa67cba59784c0d1f43e
philipantony1007 <philip.antony@ayatacommerce.com>
2024-10-30 18:38:10 +0530

    test-job

--- job/src/controllers/job.controller.ts
+++ job/src/controllers/job.controller.ts
@@ -2,56 +2,94 @@ import { Request, Response } from 'express';
 import CustomError from '../errors/custom.error';
 import { logger } from '../utils/logger.utils';
 import { allOrders } from '../repository/order.repository';
+import { mapOrderAssociations } from '../service/order.service';
+import { OrderPagedQueryResponse } from '@commercetools/platform-sdk';
+import { uploadToS3 } from '../service/s3.service';
 
-/**
- * Exposed job endpoint.
- *
- * @param {Request} _request The express request
- * @param {Response} response The express response
- * @returns
- */
+
 export const post = async (_request: Request, response: Response) => {
   try {
-    // Get the orders
-    console.log("Fetching orders...");
-    const orders = await allOrders({ sort: ['lastModifiedAt'] });
+    // Fetch the orders
+    logger.info("Fetching orders...");
 
-    // Initialize an array to hold the SKU associations
-    const associations: string[][] = [];
+    //const orders: OrderPagedQueryResponse = await allOrders({ sort: ['lastModifiedAt'] });
 
-    // Log all SKUs from line items and build the associations array
-    if (orders.results && orders.results.length > 0) {
-      orders.results.forEach((order, orderIndex) => {
-        console.log(`Order ${orderIndex + 1}:`);
-        const skuList: string[] = [];
-        order.lineItems.forEach((item, itemIndex) => {
-          const sku = item.variant.sku;
-          if (sku) {
-            skuList.push(sku);
-            console.log(`  Line Item ${itemIndex + 1} SKU: ${sku}`);
-          }
-        });
-        if (skuList.length > 0) {
-          associations.push(skuList);
-        }
-      });
-    } else {
-      console.log('No orders found.');
+    const orders = {
+      "limit": 10,
+      "count": 0,
+      "total": 0,
+      "offset": 0,
+      "results": [
+        {
+          "type": "Order",
+          "id": "f4619002-ead2-42e9-bbea-102f30c15232",
+          "version": 1,
+          "versionModifiedAt": "2024-10-17T05:16:45.125Z",
+          "lastMessageSequenceNumber": 1,
+          "createdAt": "2024-10-17T05:16:45.125Z",
+          "lastModifiedAt": "2024-10-17T05:16:45.125Z",
+          "lastModifiedBy": {
+              "isPlatformClient": true,
+              "attributedTo": {
+                  "clientId": "R2IJYphwGY8FR1vbYMkC4h-M",
+                  "source": "Import"
+              }
+          },
+          "createdBy": {
+              "isPlatformClient": true,
+              "attributedTo": {
+                  "clientId": "R2IJYphwGY8FR1vbYMkC4h-M",
+                  "source": "Import"
+              }
+          },
+          "orderNumber": "SO43702",
+          "customerEmail": "colin45@adventure-works.com",
+          "totalPrice": {
+              "type": "centPrecision",
+              "currencyCode": "USD",
+              "centAmount": 361555,
+              "fractionDigits": 2
+          },
+          "completedAt": "2019-07-01T00:00:00.000Z",
+          "orderState": "Complete",
+          "syncInfo": [],
+          "returnInfo": [],
+          "taxMode": "External",
+          "inventoryMode": "None",
+          "taxRoundingMode": "HalfEven",
+          "taxCalculationMode": "LineItemLevel",
+          "origin": "Customer",
+          "shippingMode": "Single",
+          "shipping": [],
+          "lineItems": [
+              
+          ],
+          "customLineItems": [],
+          "transactionFee": false,
+          "discountCodes": [],
+          "directDiscounts": [],
+          "itemShippingAddresses": [],
+          "refusedGifts": []
+      }
+      ]
     }
 
-    // Log the total number of orders
-    logger.info(`There are ${orders.total} orders!`);
+    // Process orders to get SKU associations
+    const associations = mapOrderAssociations(orders);
 
-    // Send the associations array as the response
-    response.status(200).json({
-      associations
-    });
+    
+    const isUploaded = await uploadToS3({ associations });
+    
+    if (isUploaded) {
+      response.status(200).json({ message: "Successfully uploaded data to S3" });
+    } else {
+      throw new CustomError(500, 'S3 upload failed');
+    }
   } catch (error) {
-    logger.error('Error retrieving all orders:', error);
-
-    throw new CustomError(
-      500,
-      'Internal Server Error - Error retrieving all orders from the commercetools SDK'
-    );
+    if (error instanceof CustomError) {
+      logger.error('Error 0 orders found:');
+      throw error;
+    }
+    throw new CustomError(500, 'Internal Server Error');
   }
 };
